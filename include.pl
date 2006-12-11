9e13e343c9a55ca8a765ebc138779dcec9146829
Michael Andreen <harv@ruin.nu>
2006-12-11 18:24:30 +0000

    forgot the include file

---
+++ include.pl
@@ -0,0 +1,39 @@
+#**************************************************************************
+#   Copyright (C) 2006 by Michael Andreen <harvATruinDOTnu>               *
+#                                                                         *
+#   This program is free software; you can redistribute it and/or modify  *
+#   it under the terms of the GNU General Public License as published by  *
+#   the Free Software Foundation; either version 2 of the License, or     *
+#   (at your option) any later version.                                   *
+#                                                                         *
+#   This program is distributed in the hope that it will be useful,       *
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#   GNU General Public License for more details.                          *
+#                                                                         *
+#   You should have received a copy of the GNU General Public License     *
+#   along with this program; if not, write to the                         *
+#   Free Software Foundation, Inc.,                                       *
+#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+#**************************************************************************/
+
+use strict;
+
+
+sub isMember {
+	return exists $ND::GROUPS{Members};
+}
+
+sub isHC {
+	return exists $ND::GROUPS{HC};
+}
+
+sub isDC {
+	return exists $ND::GROUPS{DC};
+}
+
+sub isBC {
+	return exists $ND::GROUPS{BC};
+}
+
+1;
