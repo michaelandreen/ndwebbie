701ab8c5f5b65089fe9201dbe49f9c4b7e04c624
Michael Andreen <harv@ruin.nu>
2006-12-14 12:31:14 +0000

    more or less done

--- editRaid.pl
+++ editRaid.pl
@@ -35,6 +35,100 @@ if (param('raid') =~ /^(\d+)$/){
 	my $query = $DBH->prepare(q{SELECT id,tick,waves,message,released_coords,open FROM raids WHERE id = ?});
 	$raid = $DBH->selectrow_hashref($query,undef,$1);
 }
+if (param('cmd') eq 'submit'){
+	my $query = $DBH->prepare(q{INSERT INTO raids (tick,waves,message) VALUES(?,?,'')});
+	if ($query->execute(param('tick'),param('waves'))){
+		$raid = $DBH->last_insert_id(undef,undef,undef,undef,"raids_id_seq");
+		my $query = $DBH->prepare(q{SELECT id,tick,waves,message,released_coords,open FROM raids WHERE id = ?});
+		$raid = $DBH->selectrow_hashref($query,undef,$raid);
+	}else{
+		print "<p> Something went wrong: ".$DBH->errstr."</p>";
+	}
+}
+
+if ($raid){
+	if (param('cmd') eq 'remove'){
+		$DBH->do(q{UPDATE raids SET open = FALSE, removed = TRUE WHERE id = ?},undef,$raid->{id});
+	}elsif (param('cmd') eq 'Open'){
+		if($DBH->do(q{UPDATE raids SET open = TRUE, removed = FALSE WHERE id = ?},undef,$raid->{id})){
+			$raid->{open} = 1;
+			$raid->{removed} = 0;
+		}
+	}elsif (param('cmd') eq 'Close'){
+		if ($DBH->do(q{UPDATE raids SET open = FALSE WHERE id = ?},undef,$raid->{id})){
+			$raid->{open} = 0;
+		}
+	}elsif (param('cmd') eq 'showcoords'){
+		if($DBH->do(q{UPDATE raids SET released_coords = TRUE WHERE id = ?},undef,$raid->{id})){
+			$raid->{released_coords} = 1;
+		}
+	}elsif (param('cmd') eq 'hidecoords'){
+		if($DBH->do(q{UPDATE raids SET released_coords = FALSE WHERE id = ?},undef,$raid->{id})){
+			$raid->{released_coords} = 0;
+		}
+	}elsif (param('cmd') eq 'change'){
+		$DBH->begin_work;
+		my $message = escapeHTML(param('message'));
+		$raid->{message} = $message;
+		$raid->{waves} = param('waves');
+		$raid->{tick} = param('tick');
+		unless ($DBH->do(qq{UPDATE raids SET message = ?, tick = ?, waves = ? WHERE id = ?}
+				,undef,$message,param('tick'),param('waves'),$raid->{id})){
+			print "<p> Something went wrong: ".$DBH->errstr."</p>";
+		}
+		my $sizelimit = '';
+		if (param('sizelimit') =~ /^(\d+)$/){
+			$sizelimit = "AND p.size >= $1";
+			unless ($DBH->do(qq{DELETE FROM raid_targets WHERE id IN (SELECT t.id FROM current_planet_stats p 
+	JOIN raid_targets t ON p.id = t.planet WHERE p.size < ? AND t.raid = ?)},undef,$1,$raid->{id})){
+				print "<p> Something went wrong: ".$DBH->errstr."</p>";
+			}
+		}
+		my $targets = param('targets');
+		my $addtarget = $DBH->prepare(qq{INSERT INTO raid_targets(raid,planet) (
+	SELECT ?, id FROM current_planet_stats p WHERE x = ? AND y = ? AND COALESCE(z = ?,TRUE) $sizelimit)});
+		while ($targets =~ m/(\d+):(\d+)(?::(\d+))?/g){
+			unless ($addtarget->execute($raid->{id},$1,$2,$3)){
+				print "<p> Something went wrong: ".$DBH->errstr."</p>";
+			}
+		}
+		if (param('alliance') =~ /^(\d+)$/ && $1 != 1){
+			$LOG->execute($ND::UID,"BC adding alliance $1 to raid");
+			my $addtarget = $DBH->prepare(qq{INSERT INTO raid_targets(raid,planet) (
+	SELECT ?,id FROM current_planet_stats p WHERE alliance_id = ? $sizelimit)});
+			unless ($addtarget->execute($raid->{id},$1)){
+				print "<p> Something went wrong: ".$DBH->errstr."</p>";
+			}
+		}
+		my $groups = $DBH->prepare('SELECT gid,groupname FROM groups WHERE attack');
+		my $delgroup = $DBH->prepare(q{DELETE FROM raid_access WHERE raid = ? AND gid = ?});
+		my $addgroup = $DBH->prepare(q{INSERT INTO raid_access (raid,gid) VALUES(?,?)});
+		$groups->execute();
+		while (my $group = $groups->fetchrow_hashref){
+			my $query;
+			if (param($group->{gid}) eq 'remove'){
+				$query = $delgroup;
+			}elsif(param($group->{gid}) eq 'add'){
+				$query = $addgroup;
+			}
+			if ($query){
+				unless ($query->execute($raid->{id},$group->{gid})){
+					print "<p> Something went wrong: ".$DBH->errstr."</p>";
+				}
+			}
+		}
+		unless ($DBH->commit){
+			print "<p> Something went wrong: ".$DBH->errstr."</p>";
+		}
+	}
+	if (param('removeTarget')){
+		print "test";
+		unless($DBH->do(q{DELETE FROM raid_targets WHERE raid = ? AND id = ?}
+				,undef,$raid->{id},param('removeTarget'))){
+			print "<p> Something went wrong: ".$DBH->errstr."</p>";
+		}
+	}
+}
 
 my $groups = $DBH->prepare(q{SELECT g.gid,g.groupname,raid FROM groups g LEFT OUTER JOIN (SELECT gid,raid FROM raid_access WHERE raid = ?) AS ra ON g.gid = ra.gid WHERE g.attack});
 $groups->execute($raid ? $raid->{id} : undef);
@@ -51,12 +145,14 @@ while (my $group = $groups->fetchrow_hashref){
 $BODY->param(RemoveGroups => \@remgroups);
 $BODY->param(AddGroups => \@addgroups);
 
+
 if ($raid){
+
 	$BODY->param(Raid => $raid->{id});
 	if($raid->{open}){
-		$BODY->param(Open => 'Open');
-	}else{
 		$BODY->param(Open => 'Close');
+	}else{
+		$BODY->param(Open => 'Open');
 	}
 	if($raid->{released_coords}){
 		$BODY->param(ShowCoords => 'hidecoords');
@@ -74,9 +170,8 @@ if ($raid){
 		$order = "$1 DESC";
 	}
 
-	my $targetquery = $DBH->prepare(qq{SELECT p.id,coords(x,y,z),raid,comment,size,score,value,race,planet_status AS planetstatus,relationship,comment
+	my $targetquery = $DBH->prepare(qq{SELECT r.id,coords(x,y,z),raid,comment,size,score,value,race,planet_status AS planetstatus,relationship,comment
 		FROM current_planet_stats p JOIN raid_targets r ON p.id = r.planet 
-			LEFT OUTER JOIN covop_targets c ON p.id = c.planet
 		WHERE r.raid = ?
 		ORDER BY $order});
 	$targetquery->execute($raid->{id}) or print $DBH->errstr;
