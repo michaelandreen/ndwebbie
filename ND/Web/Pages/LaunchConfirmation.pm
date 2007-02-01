aee4364ac18d6ec1c7ba9e225423ea46aafdeee7
Michael Andreen <harv@ruin.nu>
2007-02-01 15:17:56 +0000

    better parsing

--- ND/Web/Pages/LaunchConfirmation.pm
+++ ND/Web/Pages/LaunchConfirmation.pm
@@ -62,8 +62,9 @@ sub render_body {
 		}
 		my @missions;
 		$DBH->begin_work;
-		while ($missions =~ m/\S+\s+(\d+):(\d+):(\d+)\s+(\d+):(\d+):(\d+)\s+\((?:(\d+)\+)?(\d+)\).*?(?:\d+hrs\s+)?\d+mins\s+(Attack|Defend|Return|Fake Attack|Fake Defend)(.*?)(?:Launching in tick (\d+), arrival in tick (\d+)|ETA: \d+, Return ETA: (\d+))/sg){
+		while ($missions =~ m/\S+\s+(\d+):(\d+):(\d+)\s+(\d+):(\d+):(\d+)\s+\((?:(\d+)\+)?(\d+)\).*?(?:\d+hrs\s+)?\d+mins\s+(Attack|Defend|Return|Fake Attack|Fake Defend)(.*?)(?:Launching in tick (\d+), arrival in tick (\d+)|ETA: \d+, Return ETA: (\d+)|Return ETA: (\d+))/sg){
 			my %mission;
+			next if $9 eq 'Return';
 
 			my $tick = $self->{TICK}+$8;
 			$tick += $7 if defined $7;
