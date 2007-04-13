78b07c0a2aab8d21b6609c2c93fd2db5876c0d18
Michael Andreen <harv@ruin.nu>
2007-04-13 15:23:08 +0000

    oops

--- ND/Include.pm
+++ ND/Include.pm
@@ -83,7 +83,6 @@ sub intel_log {
 
 sub pa_xp {
 	my ($roids,$ascore,$avalue,$tscore,$tvalue) = @_;
-	print "@_";
 	my $bravery = (min(2,$tscore/$ascore)-0.6) * (min(2,$tvalue/$avalue)-0.4);
 	return int(max($roids * 10 * $bravery,0))
 
