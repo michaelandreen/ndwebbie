3fc40135d773a95ede229e71efb48e07fdf3c418
Michael Andreen <harv@ruin.nu>
2008-05-26 14:58:11 +0200

    Initial commit of catalyst files

---
+++ lib/NDWeb/View/TT.pm
@@ -0,0 +1,43 @@
+package NDWeb::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+	INCLUDE_PATH => [
+		NDWeb->path_to( 'root', 'src' ),
+		NDWeb->path_to( 'root', 'lib' )
+	],
+	PRE_PROCESS  => 'config/main.tt2',
+	WRAPPER      => 'site/wrapper.tt2',
+	ERROR        => 'error.tt2',
+	TIMER        => 0,
+	#DEBUG        => 'undef',
+	TEMPLATE_EXTENSION => '.tt2',
+});
+
+=head1 NAME
+
+NDWeb::View::TT - Catalyst TTSite View
+
+=head1 SYNOPSIS
+
+See L<NDWeb>
+
+=head1 DESCRIPTION
+
+Catalyst TTSite View.
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
