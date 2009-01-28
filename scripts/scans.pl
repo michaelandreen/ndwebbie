a0b5f22aa2061ef8e850d3ea04e2c68b0226cd8e
Michael Andreen <harv@ruin.nu>
2009-01-28 23:30:15 +0100

    bugfix, this is intel not fleets

--- scripts/scans.pl
+++ scripts/scans.pl
@@ -77,7 +77,7 @@ my $addfleet = $dbh->prepare(q{INSERT INTO fleets (name,mission,planet,tick,amou
 my $fleetscan = $dbh->prepare(q{INSERT INTO fleet_scans (fid,id) VALUES(?,?)});
 my $addintel = $dbh->prepare(q{INSERT INTO intel (name,mission,sender,target,tick,eta,back,amount,ingal,uid)
 	VALUES(?,?,?,?,?,?,?,?,?,-1) RETURNING id});
-my $intelscan = $dbh->prepare(q{INSERT INTO fleet_scans (intel,id) VALUES(?,?)});
+my $intelscan = $dbh->prepare(q{INSERT INTO intel_scans (intel,id) VALUES(?,?)});
 my $addships = $dbh->prepare(q{INSERT INTO fleet_ships (fid,ship,amount) VALUES(?,?,?)});
 my $addplanetscan = $dbh->prepare(q{INSERT INTO planet_scans
 	(id,tick,planet,metal_roids,metal,crystal_roids,crystal,eonium_roids,eonium
