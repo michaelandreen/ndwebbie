f3cd955b85092ea418d9c1de90be49c9f598527f
Michael Andreen <harv@ruin.nu>
2017-09-02 14:06:41 +0200

    Show target coords as soon as they are released

--- lib/NDWeb/Controller/Root.pm
+++ lib/NDWeb/Controller/Root.pm
@@ -129,8 +129,8 @@ sub listTargets : Private {
 
 	my $dbh = $c ->model;
 
-	my $query = $dbh->prepare(q{SELECT t.id, r.id AS raid, r.tick+c.wave-1 AS landingtick, 
-		(released_coords AND old_claim(timestamp)) AS released_coords, coords(x,y,z),c.launched,c.wave,c.joinable
+	my $query = $dbh->prepare(q{SELECT t.id, r.id AS raid, r.tick+c.wave-1 AS landingtick,
+		released_coords, coords(x,y,z),c.launched,c.wave,c.joinable
 FROM raid_claims c
 	JOIN raid_targets t ON c.target = t.id
 	JOIN raids r ON t.raid = r.id
