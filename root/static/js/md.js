afb55135c3bf68d8a66c95be5f94d512e8eff4da
Martijn <info@tocadovision.nl>
2019-10-21 19:35:28 +0200

    update

--- root/static/js/md.js
+++ root/static/js/md.js
@@ -1,25 +1,3 @@
-const drawer = mdc.drawer.MDCDrawer.attachTo(
-  document.querySelector(".mdc-drawer")
-);
-
-$(document).on("click", ".menu-nav-toggle", event => {
-  drawer.open = !drawer.open;
+$(document).ready(function() {
+  M.AutoInit();
 });
-
-adjustToWidth();
-
-$(window).on("resize", () => {
-  adjustToWidth();
-});
-
-function adjustToWidth() {
-  let w = window.outerWidth;
-  const drawer = mdc.drawer.MDCDrawer.attachTo(
-    document.querySelector(".mdc-drawer")
-  );
-  if (w < 768) {
-    drawer.open = false;
-  } else {
-    drawer.open = true;
-  }
-}
