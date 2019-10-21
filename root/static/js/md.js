5665d02391ffad41434073c99fbf0239e304334c
Martijn <info@tocadovision.nl>
2019-10-21 09:17:54 +0200

    MDC auto open/close

--- root/static/js/md.js
+++ root/static/js/md.js
@@ -1,9 +1,25 @@
-$(document).ready(() => {
-  const drawer = mdc.drawer.MDCDrawer.attachTo(
-    document.querySelector(".mdc-drawer--modal")
-  );
+const drawer = mdc.drawer.MDCDrawer.attachTo(
+  document.querySelector(".mdc-drawer")
+);
 
-  $(document).on("click", ".menu-nav-toggle", event => {
-    drawer.open = !drawer.open;
-  });
+$(document).on("click", ".menu-nav-toggle", event => {
+  drawer.open = !drawer.open;
 });
+
+adjustToWidth();
+
+$(window).on("resize", () => {
+  adjustToWidth();
+});
+
+function adjustToWidth() {
+  let w = window.outerWidth;
+  const drawer = mdc.drawer.MDCDrawer.attachTo(
+    document.querySelector(".mdc-drawer")
+  );
+  if (w < 768) {
+    drawer.open = false;
+  } else {
+    drawer.open = true;
+  }
+}
