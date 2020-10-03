e1450c026acb6bff904eb5a3b893dc5f270ba0a1
Michael Andreen <harv@ruin.nu>
2020-10-03 17:44:30 +0200

    Also change gain_day to bigint

--- database/bigint.sql
+++ database/bigint.sql
@@ -1,6 +1,6 @@
 BEGIN;
 	DROP VIEW alliance_resources;
-	ALTER TABLE alliance_stats ALTER COLUMN score TYPE bigint;
-	ALTER TABLE galaxies ALTER COLUMN score TYPE bigint, ALTER COLUMN value TYPE bigint;
+	ALTER TABLE alliance_stats ALTER COLUMN score TYPE bigint, ALTER COLUMN score_gain_day TYPE bigint;
+	ALTER TABLE galaxies ALTER COLUMN score TYPE bigint, ALTER COLUMN value TYPE bigint, ALTER COLUMN score_gain_day TYPE bigint, ALTER COLUMN value_gain_day TYPE bigint;
 	\ir views/alliances_resources.sql
 commit;
